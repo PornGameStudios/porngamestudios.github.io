022a7c6a90f5658f31521810855464009c5a6a4b
RobloxianProductionsTesting <porngamestudios@gmail.com>
2022-10-13 16:32:58 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,8 @@ var games =[
 ];
 
 var news =[
-	{date: "9/17/2022", game: "NaN", headline: "Website", link: "porngamestudios.github.io", text: "Official Launch"},
-	{date: "10/3/2022", game: "NA", headline: "NA", link: "porngamestudios.github.io/", text: "Official Launch"},
+	{date: "9/17/2022", game: "NaN", headline: "Website", link: "porngamestudios.github.io", text: "Test Launch"},
+	{date: "10/3/2022", game: "NA", headline: "NA", link: "porngamestudios.github.io/", text: "Page Created But Game Inaccessible"},
 ];
 
 var gamesAlreadyListed = "";
